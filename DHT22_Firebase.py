5b8856fa729ac14f2b85d66b5af0388b296245eb
Cynthia Kipruto <118298337+Chumba4209@users.noreply.github.com>
2025-08-28 23:27:42 +0300

    Create DHT22_Firebase.py

---
+++ DHT22_Firebase.py
@@ -0,0 +1,53 @@
+import network
+import time
+import urequests
+from machine import Pin
+import dht
+
+WIFI_SSID = " "
+WIFI_PASSWORD = " "
+
+# Replace with your Firebase Realtime Database URL 
+FIREBASE_URL = " "
+FIREBASE_SECRET = "YOUR_DATABASE_SECRET"   # From Firebase console -> Project Settings -> Service accounts -> Database secrets
+
+dht_pin = Pin(4)  
+sensor = dht.DHT22(dht_pin)
+
+def connect_wifi():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(WIFI_SSID, WIFI_PASSWORD)
+
+    print("Connecting to WiFi...", end="")
+    while not wlan.isconnected():
+        print(".", end="")
+        time.sleep(0.5)
+    print("\nConnected! IP:", wlan.ifconfig()[0])
+
+def send_to_firebase(temp, hum):
+    url = "{}DHT11.json?auth={}".format(FIREBASE_URL, FIREBASE_SECRET)
+    data = {"Temperature": temp, "Humidity": hum}
+    try:
+        response = urequests.patch(url, json=data)
+        print("Data sent:", data)
+        response.close()
+    except Exception as e:
+        print("Error sending to Firebase:", e)
+
+connect_wifi()
+
+while True:
+    try:
+        sensor.measure()
+        temperature = sensor.temperature()
+        humidity = sensor.humidity()
+
+        print("Temperature: {}°C, Humidity: {}%".format(temperature, humidity))
+
+        send_to_firebase(temperature, humidity)
+
+    except Exception as e:
+        print("Sensor error:", e)
+
+    time.sleep(10)  
